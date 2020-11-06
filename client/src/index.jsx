da1128faf2737f552a4f352a92a59cc3671ee7e5
Larbi <ferhaoui.20044@gmail.com>
2020-11-06 22:00:32 +0100

    edit for started Project

---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RectDom from 'react-dom'
+
+import App from './app'
+
+RectDom.render(<App />, document.getElementById('root'))
---
+++ server/index.js
@@ -0,0 +1,26 @@
+/* import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors' */
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+const CONNECT_URL = 'mongodb://localhost:27017/memories';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, console.log('Server Running on port ', PORT)))
+    .catch(err => console.log(err))
+
+mongoose.set('useFindAndModify', false)
+
+// https://www.mongodb.com/cloud/atlas
